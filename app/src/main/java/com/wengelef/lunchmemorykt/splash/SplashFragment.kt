745d69c690172e9b30bcb4f0f07a986285adbaf7
Florian Wengelewski <wengelef@gmail.com>
2018-09-17 15:56:30 +0200

    init : add basic project

---
+++ app/src/main/java/com/wengelef/lunchmemorykt/splash/SplashFragment.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) wengelef 2018
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wengelef.lunchmemorykt.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wengelef.lunchmemorykt.BaseFragment
+import com.wengelef.lunchmemorykt.R
+
+class SplashFragment : BaseFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fr_splash, container, false)
+    }
+}
